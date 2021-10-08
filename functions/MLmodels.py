7283d1d2c08215d0808b19fd54e45ea9a04e86af
Tommy-Lin-Taiwan <78583266+Tommy-Lin-Taiwan@users.noreply.github.com>
2021-10-09 00:00:35 +0800

    create

---
+++ functions/MLmodels.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import sklearn as sk
+import autokeras as ak
+import tensorflow as tf
+from tensorflow import keras
+import math
+import random
+
+
+# give the possibility of the success
+def linearNN(input, preload=True, ):
+    if(preload):
+        return None
+    else:
+        return None
